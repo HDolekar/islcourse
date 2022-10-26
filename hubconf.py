b3d7888da19e8d5ed6d2fc1e786830677698c03a
Himanshu DOLEKAR <43377009+cs21m007iittp@users.noreply.github.com>
2022-10-26 11:03:27 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -33,24 +33,24 @@ classes = [
 
 # Define model
 
-class cs21m007nn(nn.Module):
-    def __init__(self,num_classes=10):
-        super(cs21m007nn, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=5, padding=2),
-            nn.RelU()
-            nn.MaxPool2d(2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(2))
-        self.fc = nn.Linear(7*7*32, 10)
+# class cs21m007nn(nn.Module):
+#     def __init__(self,num_classes=10):
+#         super(cs21m007nn, self).__init__()
+#         self.layer1 = nn.Sequential(
+#             nn.Conv2d(1, 16, kernel_size=5, padding=2),
+#             nn.RelU()
+#             nn.MaxPool2d(2))
+#         self.layer2 = nn.Sequential(
+#             nn.Conv2d(16, 32, kernel_size=5, padding=2),
+#             nn.ReLU(),
+#             nn.MaxPool2d(2))
+#         self.fc = nn.Linear(7*7*32, 10)
         
-    def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.fc(out)
-        return out
+#     def forward(self, x):
+#         out = self.layer1(x)
+#         out = self.layer2(out)
+#         out = self.fc(out)
+#         return out
 
     
 # Define model simle model 
