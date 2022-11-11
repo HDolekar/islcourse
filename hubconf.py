3c19870109019ce1db9c70809d92fecc58560f67
Himanshu DOLEKAR <43377009+cs21m007iittp@users.noreply.github.com>
2022-11-11 20:52:37 +0530

    correcting error in test f() definition

--- hubconf.py
+++ hubconf.py
@@ -152,7 +152,7 @@ def train(trainloader,my_model,epochs,learning_rate=1e-3):
 
 
 
-def test(dataloader, model, loss_fn):
+def test(dataloader, model):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     num_classes= len(dataloader.dataset.classes)
