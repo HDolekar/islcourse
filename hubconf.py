a715a4577719901bc43b1efb8acacbedc90b10c2
Himanshu DOLEKAR <43377009+cs21m007iittp@users.noreply.github.com>
2022-11-21 12:34:10 +0530

    errors

--- hubconf.py
+++ hubconf.py
@@ -120,7 +120,7 @@ def get_metrics(model1,X,y):
   #print(prec)
   f1=f1_score(y_test,y_pred_test,average='macro')
   
-  fpr, tpr, thresholds = metrics.roc_curve(y, ypred_test, pos_label=2)
+  fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred_test, pos_label=2)
   auc = metrics.auc(fpr, tpr)
   
   #auc=roc_auc_score(y_test,y_pred_test,multi_class='ovr')
@@ -130,60 +130,61 @@ def get_metrics(model1,X,y):
   return acc, prec, rec, f1, auc
 
 
+# part 2b ...
 
 
-# def get_paramgrid_lr():
-#   # you need to return parameter grid dictionary for use in grid search cv
-#   # penalty: l1 or l2
+def get_paramgrid_lr():
+  # you need to return parameter grid dictionary for use in grid search cv
+  # penalty: l1 or l2
   
-#   lr_param_grid = {'penalty' : ['l1','l2']}
+  lr_param_grid = {'penalty' : ['l1','l2']}
 
   
-#   # refer to sklearn documentation on grid search and logistic regression
-#   # write your code here...
-#   return lr_param_grid
+  # refer to sklearn documentation on grid search and logistic regression
+  # write your code here...
+  return lr_param_grid
 
-# def get_paramgrid_rf():
-#   # you need to return parameter grid dictionary for use in grid search cv
-#   # n_estimators: 1, 10, 100
-#   # criterion: gini, entropy
-#   # maximum depth: 1, 10, None  
-#   rf_param_grid = { 'n_estimators' : [1,10,100],'criterion' :["gini", "entropy"], 'max_depth' : [1,10,None]  }
-#   # refer to sklearn documentation on grid search and random forest classifier
-#   # write your code here...
-#   return rf_param_grid
+def get_paramgrid_rf():
+  # you need to return parameter grid dictionary for use in grid search cv
+  # n_estimators: 1, 10, 100
+  # criterion: gini, entropy
+  # maximum depth: 1, 10, None  
+  rf_param_grid = { 'n_estimators' : [1,10,100],'criterion' :["gini", "entropy"], 'max_depth' : [1,10,None]  }
+  # refer to sklearn documentation on grid search and random forest classifier
+  # write your code here...
+  return rf_param_grid
 
-# def perform_gridsearch_cv_multimetric(model=None, param_grid=None, cv=5, X=None, y=None, metrics=['accuracy','roc_auc']):
+def perform_gridsearch_cv_multimetric(model=None, param_grid=None, cv=5, X=None, y=None, metrics=['accuracy','roc_auc']):
   
-#   # you need to invoke sklearn grid search cv function
-#   # refer to sklearn documentation
-#   # the cv parameter can change, ie number of folds  
+  # you need to invoke sklearn grid search cv function
+  # refer to sklearn documentation
+  # the cv parameter can change, ie number of folds  
   
-#   # metrics = [] the evaluation program can change what metrics to choose
+  # metrics = [] the evaluation program can change what metrics to choose
   
-#   grid_search_cv = None
-#   # create a grid search cv object
-#   # fit the object on X and y input above
-#   # write your code here...
+  grid_search_cv = None
+  # create a grid search cv object
+  # fit the object on X and y input above
+  # write your code here...
   
-#   # metric of choice will be asked here, refer to the-scoring-parameter-defining-model-evaluation-rules of sklearn documentation
+  # metric of choice will be asked here, refer to the-scoring-parameter-defining-model-evaluation-rules of sklearn documentation
   
-#   # refer to cv_results_ dictonary
-#   # return top 1 score for each of the metrics given, in the order given in metrics=... list
+  # refer to cv_results_ dictonary
+  # return top 1 score for each of the metrics given, in the order given in metrics=... list
   
-#   top1_scores = []
+  top1_scores = []
   
   
-#   if X.ndim > 2:
-#       n_samples = len(X)
-#       X= X.reshape((n_samples, -1))
+  if X.ndim > 2:
+      n_samples = len(X)
+      X= X.reshape((n_samples, -1))
       
-#   for score in metrics:
-#       grid_search_cv = GridSearchCV(model,param_grid,scoring = score,cv=cv)
-#       grid_search_cv.fit(X,y)
-#       top1_scores.append(grid_search_cv.best_estimator_.get_params())
+  for score in metrics:
+      grid_search_cv = GridSearchCV(model,param_grid,scoring = score,cv=cv)
+      grid_search_cv.fit(X,y)
+      top1_scores.append(grid_search_cv.best_estimator_.get_params())
   
-#   return top1_scores
+  return top1_scores
 
 ###### PART 3 ######
 
@@ -243,7 +244,10 @@ def get_metrics(model1,X,y):
 # def get_mnist_tensor():
 #   # download sklearn mnist
 #   # convert to tensor
-#   X, y = None, None
+#   X_np, y_np = get_data_mnist()
+
+#   X = torch.tensor(X_np)
+#   y = torch.tensor(y_np)
 #   # write your code
 #   return X,y
 
